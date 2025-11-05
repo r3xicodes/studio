5a203ddb0b67b561067b454f41a8012cefb28002
Riley <devilroyalty666@gmail.com>
2025-11-05 15:36:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1504783637-65979",
+  "appId": "1:357836176186:web:9abc36191131b943f7ee03",
+  "apiKey": "AIzaSyDDAIKAAfSp7WpcPFVNzXN_Vi8OqvOi7fc",
+  "authDomain": "studio-1504783637-65979.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "357836176186"
+};
